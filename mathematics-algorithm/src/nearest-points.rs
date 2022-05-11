43864d9cc867436836030058d6c117e378c67246
m3y <ma3ya.ozw+github@gmail.com>
2022-05-12 01:07:58 +0900

    034 - Nearest Points (refactoring)

--- mathematics-algorithm/src/nearest-points.rs
+++ mathematics-algorithm/src/nearest-points.rs
@@ -6,15 +6,15 @@ fn main() {
         xy: [(f64, f64);n],
     }
 
-    let mut ans: Vec<f64> = Vec::new();
+    let mut ans: f64 = 10000000.0;
     for i in 0..n {
         for j in i..n {
             if i == j {
                 continue;
             }
-            ans.push(((xy[j].0 - xy[i].0).powi(2) + (xy[j].1 - xy[i].1).powi(2)).sqrt());
+            ans = ans.min(((xy[j].0 - xy[i].0).powi(2) + (xy[j].1 - xy[i].1).powi(2)).sqrt());
         }
     }
 
-    println!("{}", ans.iter().fold(0.0 / 0.0, |m, v| v.min(m)));
+    println!("{}", ans);
 }
